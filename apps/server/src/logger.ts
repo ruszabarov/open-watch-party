abb11d9d761f08047879e4ad5f8fa85164d791cd
Ruslan Zabarov <ruszabarov@gmail.com>
2026-04-28 20:59:35 -0400

    log level

--- apps/server/src/logger.ts
+++ apps/server/src/logger.ts
@@ -2,6 +2,9 @@ import pino from 'pino';
 
 export const logger = pino({
   level: process.env['NODE_ENV'] === 'test' ? 'silent' : (process.env['LOG_LEVEL'] ?? 'info'),
+  formatters: {
+    level: (label) => ({ level: label }),
+  },
 });
 
 export function getLogError(error: unknown): { name?: string; message: string } {
